13eb35ed28e2e4b9ea4c1b7281350bb85e78f3a1
foxtroit7 <rakeshbihari5301@gmail.com>
2025-04-29 10:52:26 +0530

    updated

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -5,7 +5,5 @@ const { JWT_SECRET_KEY } = require("../config/db");
 exports.generateToken = (user_id, isAdmin = false) => {
     const role = isAdmin ? "admin" : "user";  // Set role dynamically
     const payload = { user_id, role };
-
-    // Generate token with 48-hour expiration
-    return jwt.sign(payload, JWT_SECRET_KEY, { expiresIn: "96h" });
+    return jwt.sign(payload, JWT_SECRET_KEY);
 };
